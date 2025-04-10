f32cb800482d98d47dc00c0ccca8c6b74dc2b19c
CelineBoutinon <143210563+CelineBoutinon@users.noreply.github.com>
2025-04-10 22:01:31 +0200

    update requirements

--- api.py
+++ api.py
@@ -9,16 +9,6 @@ import pandas as pd
 
 app = Flask(__name__)
 
-@app.route('/update_server', methods=['POST'])
-def webhook():
-    if request.method == 'POST':
-        repo = git.Repo('path/to/git_repo')
-        origin = repo.remotes.origin
-        origin.pull()
-        return 'Updated PythonAnywhere successfully', 200
-    else:   
-        return 'Wrong event type', 400
-
 # Load client test data
 client_data=pd.read_csv('X_test_final.csv')
 
