50ac928c70d15308af4f4e17f82e12ae094545d6
CelineBoutinon <143210563+CelineBoutinon@users.noreply.github.com>
2025-04-10 22:47:28 +0200

    add streamlit app

---
+++ streamlit_cloud_app_v6.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import requests
+
+# Streamlit UI
+st.title("Welcome to the ")
+st.image("logo.png")
+st.title("Credit Scoring App!")
+
+# Select client credit application reference
+selected_value = st.select_slider(
+    "Select a client credit application reference:",
+    options=range(1, 46128)
+)
+
+# Display the selected value
+st.write(f"You selected client application: {selected_value}")
+
+# IMPORTANT: In advanced settings, choose Python 3.10 when deploying the app in Streamlit Cloud
+# to avoid errors related to distutils (discontinued in Python 3.12 onwards).
+
+# Send a POST request to the API using the selected value
+response = requests.post(f"https://credit-scoring-api-0p1u.onrender.com/{selected_value}")
+
+# Display the response from the API (optional)
+if response.status_code == 200:
+    st.write(f"API Response: {response.text}")
+else:
+    st.error(f"Failed to fetch data. Status code: {response.status_code}")
+
+# Main function placeholder (optional)
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
