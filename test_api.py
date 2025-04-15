ed2c1f3c7170f3c789ecd208a59105fcf0b5b32d
CelineBoutinon <143210563+CelineBoutinon@users.noreply.github.com>
2025-04-15 16:31:43 +0200

    update comments in test file

--- test_api.py
+++ test_api.py
@@ -1,10 +1,7 @@
 # run from the command line with py -m pytest test_api.py -v -s
 
-# import os
-# import sys
 import joblib
 import pandas as pd
-# from flask import Flask, jsonify, request
 import app
 import pytest
 import requests
@@ -29,12 +26,12 @@ def test_client_demographics():
     response = requests.get(f"{base_url}/client/{random_client_id}")
     assert response.status_code == 200
 
-# Test that the client demographics endpoint returns an error for invalid client ID
+# Test that the client demographics endpoint returns an error for an invalid client ID
 def test_client_demographics_invalid_id():
     response = requests.get(f"{base_url}/client/46129")
     assert response.status_code == 404
 
-# Test that the prediction endpoint returns an error for invalid client ID
+# Test that the prediction endpoint returns an error for an invalid client ID
 def test_prediction_invalid_id():
     response = requests.get(f"{base_url}/predict/46129")
     assert response.status_code == 500
@@ -48,38 +45,26 @@ def test_prediction_json_structure():
     expected_keys = ['Client id', 'Client default probability', 'Class', 'Decision', 'Key Decision Factors', 'Expected Shap Value', 'Shap values client']
     assert all(key in data for key in expected_keys)
 
+# Test that the champion MLFlow model loads
 def test_model_loading():
     model = joblib.load('final_model.joblib')
     assert model is not None, "Error loading model."
 
+# Test that the client test data csv file loads
 def test_csv_loading():
     df = pd.read_csv('X_test_final.csv')
     assert not df.empty, "Error loading csv file."
 
-# Test that the prediction returns a default probability
+# Test that the prediction returns a default probability and that it is a number
 def test_prediction():
-    # Load test data
     df = pd.read_csv('X_test_final.csv')
-
     random_client_id = random.randint(1, 46128)
-
-    # Get the index from the first row of your test data
-    index_value = df.index[random_client_id]  # Get the first index value
-
+    index_value = df.index[random_client_id]
     with app.test_client() as client:
-        # Make a request to your prediction endpoint, matching your API route
-        response = client.get(f'/predict/{index_value}')  # Use .get() and f-string
-
-        # Parse the JSON response
+        response = client.get(f'/predict/{index_value}')
         data = json.loads(response.data)
-
-        # Extract the prediction (adjust key to match your API response)
-        prediction = data['Client default probability']  # Assuming your API returns {'prediction': value}
-
-        # Assert that the prediction is not None
+        prediction = data['Client default probability']
         assert prediction is not None, "Failed to predict application outcome for client."
-
-        # Optionally, add more specific assertions about the prediction value
         assert isinstance(prediction, (int, float)), "Prediction should be a number."
 
 
